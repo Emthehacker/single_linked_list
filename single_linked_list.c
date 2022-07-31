7bd4ff8ab9e397df6d6d79ea37dd09f999d26038
Monametsi <elboncaptain@gmail.com>
2022-07-31 08:46:12 +0200

    This is my second commit

--- single_linked_list.c
+++ single_linked_list.c
@@ -1,3 +1,4 @@
+//This is an example code of creating a single linked list.
 //Creating a single linked list.
 //Node has data and link
 #include <stdio.h>
@@ -7,14 +8,21 @@ struct node
 {
     int data;
     struct node *link;
-};
+}*head, *ptr1;
 
 int main()
 {
-    struct node *head = NULL;
+    head = NULL;
     head = (struct node *)malloc(sizeof(struct node));
     (*head).data = 45;
     (*head).link = NULL;
+    printf("%d\n", head -> data);
 
-    printf("%d", head -> data);
+    ptr1 = NULL;
+    ptr1 = (struct node *)malloc(sizeof(struct node));
+    ptr1 -> data = 45;
+    ptr1 -> link = NULL;
+    head -> link = ptr1;
+    printf("%d", ptr1 -> data);
+    return 0;
 }
